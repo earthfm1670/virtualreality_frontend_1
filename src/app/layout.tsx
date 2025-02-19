3e880dea18e4858f49b1c797b80201465243b3c3
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2025-02-20 03:29:55 +0700

    add: Metadata type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type React from "react";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Todo list",
   description: "Dark themed todo list",
 };
