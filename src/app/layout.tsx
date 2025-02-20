fdc576d32048ef4fd08c6ce01bff894b6129be10
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2025-02-20 11:11:18 +0700

    add: setup project main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,18 +6,21 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Todo list",
-  description: "Dark themed todo list",
+  title: "BuyMovie - VirtualReality Movie Store",
+  description:
+    "Browse and purchase your favorite movie online at the price that is right for you",
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-900 text-gray-100`}>
+      <body
+        className={`${inter.className} min-h-screen bg-gray-900 text-gray-100`}
+      >
         {children}
       </body>
     </html>
